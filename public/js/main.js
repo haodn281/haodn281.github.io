396771f53dd9e603ea11c19d12721b83317eba5b
haodn281 <haodn281@gmail.com>
2018-05-11 12:03:15 +0700

    update 11/05/2018

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,42 @@
-let a = 1, b = 2, c = 3;
+const JQUERY = `<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>`;
+console.log(location.pathname);
+const NAVIGATION_BAR = (`
+    <div>
+        <a
+        style="
+            color: white;
+            padding: 20px;
+            display: inline-block;
+            background-color: darkorange;
+            text-decoration:none;
+        "
+        href="./page_1.html">page_1</a>
+        <a
+        style="
+            color: white;
+            padding: 20px;
+            display: inline-block;
+            background-color: cornflowerblue;
+            text-decoration:none;
+        "
+        href="./page_2.html">page_2</a>
+        <a
+        style="
+            color: white;
+            padding: 20px;
+            display: inline-block;
+            background-color: blueviolet;
+            text-decoration:none;
+        "
+        href="./page_3.html">page_3</a>
+        <a
+        style="
+            color: white;
+            padding: 20px;
+            display: inline-block;
+            background-color: brown;
+            text-decoration:none;
+        "
+        href="./page_4.html">page_4</a>
+    </div>
+`);

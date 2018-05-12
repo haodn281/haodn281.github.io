d1b1c511bc8bfdf753ec7f9bfb879c730e2f02e6
haodn281 <haodn281@gmail.com>
2018-05-12 16:07:05 +0700

    Lam done project

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,22 @@
-const JQUERY = `<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>`;
-console.log(location.pathname);
-const NAVIGATION_BAR = (`
-    <div>
+const HEADER = `
+<div style="background-color: orange; position: fixed; width: 100%; padding: 0 100px 0 100px;z-index:1">
+    <img src="https://adjob.asia/wp-content/uploads/2016/12/adjob_small.png" style="display: inline-block;">
+    <a href="https://fb.me" target="_blank"><i class="fab fa-lg fa-facebook pull-right" style="font-size: 20px; color:#3b5bdb;background-color:#ffffff;margin: 10px 0 10px 0"></i></a>
+    <a href="https://youtube.com" target="_blank"><i class="fab fa-youtube pull-right" style="font-size: 20px; color: red; margin: 10px 10px 10px 0"></i></a>
+    <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram pull-right" style="font-size: 20px;color: red; margin: 10px 10px 10px 0"></i>
+</div>
+`;
 
+const NAVIGATION_BAR = (`
+    <div style="padding:43px 0 0 0;margin:0 0 10px 0;background-color:#a70e1a">
+    <a
+    style="
+        color: white;
+        padding: 20px;
+        display: inline-block;
+        text-decoration:none;
+    "
+    href="./index.html">index</a>
 
     
         <a
@@ -10,7 +24,6 @@ const NAVIGATION_BAR = (`
             color: white;
             padding: 20px;
             display: inline-block;
-            background-color: darkorange;
             text-decoration:none;
         "
         href="./page_1.html">page_1</a>
@@ -19,7 +32,6 @@ const NAVIGATION_BAR = (`
             color: white;
             padding: 20px;
             display: inline-block;
-            background-color: cornflowerblue;
             text-decoration:none;
         "
         href="./page_2.html">page_2</a>
@@ -28,7 +40,6 @@ const NAVIGATION_BAR = (`
             color: white;
             padding: 20px;
             display: inline-block;
-            background-color: blueviolet;
             text-decoration:none;
         "
         href="./page_3.html">page_3</a>
@@ -37,9 +48,33 @@ const NAVIGATION_BAR = (`
             color: white;
             padding: 20px;
             display: inline-block;
-            background-color: brown;
             text-decoration:none;
         "
         href="./page_4.html">page_4</a>
     </div>
 `);
+
+document.addEventListener('DOMContentLoaded', e => {
+    console.log(e);
+    var head = document.head,
+        body = document.body;
+    var bootstrap = {
+        js: document.createElement('script'),
+        css: document.createElement('link')
+    },
+    fontawesome = document.createElement('link')
+    //<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    bootstrap.js.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
+    bootstrap.css.rel = 'stylesheet';
+    bootstrap.css.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
+    fontawesome.rel = 'stylesheet';
+    fontawesome.href = 'https://use.fontawesome.com/releases/v5.0.13/css/all.css';
+
+    head.appendChild(bootstrap.js);
+    head.appendChild(bootstrap.css);
+    head.appendChild(fontawesome)
+
+    //components
+    body.querySelector('div#navigation-bar').innerHTML = NAVIGATION_BAR;
+    body.querySelector('div#header').innerHTML = HEADER;
+});
